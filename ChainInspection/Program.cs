738a916c261396aad6f50cf73fda8bc55e3b7863
Joe Reid <jreid@vnet.net>
2014-06-06 17:05:29 -0500

    finished ChainInspection

--- ChainInspection/Program.cs
+++ ChainInspection/Program.cs
@@ -15,16 +15,19 @@ namespace ChainInspection
                 while (!reader.EndOfStream)
                 {
                     string line = reader.ReadLine();
+                    //Console.WriteLine(line);
                     if (string.IsNullOrEmpty(line))
                         continue;
                     // check to make sure there is a BEGIN and an END first
                     if (!Regex.IsMatch(line, "BEGIN-"))
                     {
+                        //Console.WriteLine("BAD - no begin");
                         Console.WriteLine("BAD");
                         continue;
                     }
                     if (!Regex.IsMatch(line, "-END"))
                     {
+                        //Console.WriteLine("BAD - no end");
                         Console.WriteLine("BAD");
                         continue;
                     }
@@ -49,20 +52,55 @@ namespace ChainInspection
                             break;
                         }
                     }
+                    // special case - 1 element chain of BEGIN-END
+                    if ((links.Count == 0) && (linkEnd == "END"))
+                    {
+                        Console.WriteLine("GOOD");
+                        continue;
+                    }
                     while (links.Count != 0)
                     {
                         // find next link
                         // get linkEnd
                         // remove link
                         // if there is no link we have a problem!!!
+                        bool lostSoul = false;
+                        foreach (string link in links)
+                        {
+                            string[] splits = link.Split('-');
+                            string begin = splits[0];
+                            string end = splits[1];
+                            if (begin == linkEnd)
+                            {
+                                linkEnd = end;
+                                links.Remove(link);
+                                lostSoul = false;
+                                break;
+                            }
+                            // if we get to end of chain (without breaking)
+                            lostSoul = true;
+                        }
+                        if ((links.Count == 0) && (linkEnd == "END"))
+                        {
+                            Console.WriteLine("GOOD");
+                            break;
+                        }
+                        else if (lostSoul)
+                        {
+                            //Console.WriteLine("BAD - not at end, links still left");
+                            Console.WriteLine("BAD");
+                            break;
+                        }
+                        else if (linkEnd == "END")
+                        {
+                            //Console.WriteLine("BAD - still links left");
+                            Console.WriteLine("BAD");
+                            break;
+                        }
                     }
-                    if ((links.Count != 0) && (linkEnd != "END"))
-                    {
-                        Console.WriteLine("BAD");
-                    }
-                    else
-                        Console.WriteLine("GOOD");
+                    
                 }
+            //Console.ReadLine();
         }
     }
 }
