83a9b68767df7330e310ff92e2247dda16918e8e
Joe Reid <jreid@vnet.net>
2014-06-08 20:36:39 -0500

    inital solution to Pass Triangle

--- PassTriangle/Program.cs
+++ PassTriangle/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.IO;
@@ -8,50 +9,79 @@ namespace PassTriangle
 {
     class Program
     {
+
+        // thanks to
+        // http://hamidj.wordpress.com/tag/codeeval-pass-triangle/
+
+        public static List<List<int>> tree = new List<List<int>>();
+
         static void Main(string[] args)
         {
-            int sum = 0;
-            int index = 0;
             using (StreamReader reader = File.OpenText(args[0]))
                 while (!reader.EndOfStream)
                 {
                     string line = reader.ReadLine();
                     if (string.IsNullOrEmpty(line))
                         continue;
-                    Console.WriteLine(line);
-                    string[] numberS = line.Trim().Split(' ');
-                    if (sum == 0)
-                    {
-                        sum = Convert.ToInt32(numberS[0]);
-                        continue;
-                    }
-                    int[] numbers = new int[numberS.Length];
-                    for (int i = 0; i < numbers.Length; i++)
-                    {
-                        numbers[i] = Convert.ToInt32(numberS[i]);
-                    }
-                    int left = numbers[index];
-                    int right = numbers[index + 1];
-                    Console.WriteLine("S:{0} L:{1} R:{2} I:{3}", sum, left, right, index);
-                    if (left > right)
-                    {
-                        Console.WriteLine("adding left");
-                        sum += left;
-                    }
-                    else if (right > left)
-                    {
-                        Console.WriteLine("adding right");
-                        sum += right;
-                        index++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Fucking equal!");
-                        break;
-                    }
+                    List<int> theLine = Array.ConvertAll(line.Trim().Split(' '), element => Convert.ToInt32(element)).ToList();
+                    tree.Add(theLine);
                 }
-            Console.WriteLine(sum);
+            Console.WriteLine(sumOfTree());
             Console.ReadLine();
         }
+
+        private static int sumOfTree()
+        {
+            int depthOfTree = tree.Count;
+            for (int i = depthOfTree - 2; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (tree[i + 1][j] > tree[i + 1][j + 1])
+                        tree[i][j] += tree[i + 1][j];
+                    else
+                        tree[i][j] += tree[i + 1][j + 1];
+                }
+            }
+            return tree[0][0];
+        }
     }
+
+    //class nothing
+    //{
+    //    void method_test()
+    //    {     
+    //        Console.WriteLine(line);
+    //                string[] numberS = line.Trim().Split(' ');
+    //                if (sum == 0)
+    //                {
+    //                    sum = Convert.ToInt32(numberS[0]);
+    //                    continue;
+    //                }
+    //                int[] numbers = new int[numberS.Length];
+    //                for (int i = 0; i < numbers.Length; i++)
+    //                {
+    //                    numbers[i] = Convert.ToInt32(numberS[i]);
+    //                }
+    //                int left = numbers[index];
+    //                int right = numbers[index + 1];
+    //                Console.WriteLine("S:{0} L:{1} R:{2} I:{3}", sum, left, right, index);
+    //                if (left > right)
+    //                {
+    //                    Console.WriteLine("adding left");
+    //                    sum += left;
+    //                }
+    //                else if (right > left)
+    //                {
+    //                    Console.WriteLine("adding right");
+    //                    sum += right;
+    //                    index++;
+    //                }
+    //                else
+    //                {
+    //                    Console.WriteLine("Fucking equal!");
+    //                    break;
+    //                }
+    //    }
+    //}
 }
